b946d1c50a9f3ab5455624aa4bfa410edc16c74f
Daniel Konhauser <dkonhauser@gmail.com>
2015-08-28 04:08:54 -0700

    Update acc_imgs_shuffle.py

--- acc_imgs_shuffle.py
+++ acc_imgs_shuffle.py
@@ -6,17 +6,17 @@ import adxl345
 from SimpleCV import *
 print __doc__
 
-#Settings
+
 my_images_pathA = "/home/pi/imageA" #put your image path here if you want to override current directory
 extension = "*.jpg"
 my_images_pathB = "/home/pi/imageB"
 accel = adxl345.ADXL345()
-#axes = accel.getAxes(True)
+axes = accel.getAxes(True)
 
 pathA = my_images_pathA
 pathB = my_images_pathB
   
-imgs = list() #load up an image list
+imgs = list() 
 directoryA = os.path.join(pathA, extension)
 filesA = glob.glob(directoryA)
 
@@ -25,16 +25,33 @@ directoryB = os.path.join(pathB, extension)
 filesB = glob.glob(directoryB)
 
 while True:
-
+        axes = accel.getAxes(True)
+        x = axes['x']
+        y = axes['y']
+        z = axes['z']
+        
         random.shuffle(filesA)
         random.shuffle(filesB)
-        new_imgA = Image(filesA[1])
         disp = Display((800,800))
         pygame.mouse.set_visible(False)
+        
+        new_imgA = Image(filesA[1])
         new_imgB = Image(filesB[1])
-        newer_img = new_imgA + new_imgB
-        disp.writeFrame(newer_img)
+        
+        if x > .95:
+             newer_img = new_ImgA.binarize() + new_imgB
+             disp.writeFrame(newer_img)
 
+        elif z < -.65:
+             newer_img = new_ImgA + new_ImgB.invert()
+             disp.writeFrame(newer_img)
+             
+        else:
+             newer_img = new_ImgA + new_ImgB
+             disp.writeFrame(newer_img)
+            
+          
+             
 
 
 
