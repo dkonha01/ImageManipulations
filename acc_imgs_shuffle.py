e123182f81143725a59110cfa5ab5d821f79779d
Daniel Konhauser <dkonhauser@gmail.com>
2015-09-01 16:48:36 -0700

    add folder imageC
    use a black jpeg in the else statement

--- acc_imgs_shuffle.py
+++ acc_imgs_shuffle.py
@@ -9,11 +9,16 @@ print __doc__
 my_images_pathA = "/home/pi/imageA" #put your image path here if you want to override current directory
 extension = "*.jpg"
 my_images_pathB = "/home/pi/imageB"
+my_images_pathC = "/home/pi/imageC"
 accel = adxl345.ADXL345()
 axes = accel.getAxes(True)
 
+
+// add solids including black to imageC folder
+
 pathA = my_images_pathA
 pathB = my_images_pathB
+pathC = my_images_pathC
   
 imgs = list() #load up an image list
 directoryA = os.path.join(pathA, extension)
@@ -22,6 +27,10 @@ filesA = glob.glob(directoryA)
 imgs = list()
 directoryB = os.path.join(pathB, extension)
 filesB = glob.glob(directoryB)
+
+imgs = list()
+directoryC = os.path.join(pathC, extension)
+filesC = glob.glob(directoryC)
 
 while True:
 
@@ -32,13 +41,18 @@ while True:
 
         random.shuffle(filesA)
         random.shuffle(filesB)
+        random.shuffle(filesC)
         new_imgA = Image(filesA[1])
         disp = Display((800,800))
         pygame.mouse.set_visible(False)
         new_imgB = Image(filesB[1])
+        new_imgC = Image(filesC[1])
+        
 
         if x > .95:
-             newer_img = new_imgA.binarize() 
+             //newer_img = new_imgA.binarize() 
+             //disp.writeFrame(newer_img)
+             newer_img = new_imgA + new_imgB
              disp.writeFrame(newer_img)
 
         elif y < -.65:
@@ -46,7 +60,7 @@ while True:
              disp.writeFrame(newer_img)
              
         else:
-             newer_img = new_imgA + new_imgB
+             newer_img = new_imgC
              disp.writeFrame(newer_img)
 
 
